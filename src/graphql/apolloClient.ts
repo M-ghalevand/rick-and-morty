f660944961169141c798b3f9286f7add8133fa78
Manouchehr Ghalevand <m.ghalevand@outlook.com>
2023-07-27 22:15:27 +0330

    Add graphql folder to src, config apolloClient, add ApolloProvider to app root

---
+++ src/graphql/apolloClient.ts
@@ -0,0 +1,9 @@
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+
+const httpLink = new HttpLink({
+  uri: "https://rickandmortyapi.com/graphql",
+});
+export const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
