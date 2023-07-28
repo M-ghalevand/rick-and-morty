a2014ee47862ae9a1c0c280a65659cfeb2e5b0a5
Manouchehr Ghalevand <m.ghalevand@outlook.com>
2023-07-28 11:37:16 +0330

    added Button Clear all, added action handleClearAll

--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -1,9 +1,34 @@
+import { FC, lazy } from "react";
 import style from "./filter.module.scss";
-import Accordion from "../accordion/Accordion";
-const Filter = () => {
+import { Button } from "@mui/material";
+import useSelectors from "../../hooks/useSelectors";
+import { clearAll, fetchCharacters } from "../../store/slice/AppSlice";
+import { useAppDispatch } from "../../store/ConfigureStore";
+
+const Accordion = lazy(() => import("../accordion/Accordion"));
+
+const Filter: FC = () => {
+  const dispatch = useAppDispatch();
+  const { selectCharactersStatus, selectGender, selectSpecie } = useSelectors();
+  const handleClearAll = () => {
+    dispatch(clearAll());
+    dispatch(fetchCharacters({}));
+  };
   return (
     <div className={style.filterRoot}>
       <p className={style.title}>filters</p>
+      {(selectCharactersStatus || selectGender || selectSpecie) && (
+        <Button
+          variant={"outlined"}
+          size="small"
+          color={"error"}
+          sx={{ position: "absolute", top: "-30px", right: "20px" }}
+          onClick={handleClearAll}
+        >
+          Clear all
+        </Button>
+      )}
+
       <div className={style.line}></div>
       <Accordion />
     </div>
