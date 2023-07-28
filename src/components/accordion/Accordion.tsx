b0f51506bc2d618dee2214dc9af26060b6b995c4
Manouchehr Ghalevand <m.ghalevand@outlook.com>
2023-07-28 10:52:39 +0330

    Created  accordion component, styled each as a module and added it to the filter file.

---
+++ src/components/accordion/Accordion.tsx
@@ -0,0 +1,151 @@
+import type { FC } from "react";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import {
+  Accordion as MUIAccordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+  Button,
+} from "@mui/material/";
+
+import style from "./accordion.module.scss";
+import statusData from "../../data/status.json";
+import speciesData from "../../data/species.json";
+import gendersData from "../../data/genders.json";
+import { useAppDispatch } from "../../store/ConfigureStore";
+import {
+  fetchCharacters,
+  setCharactersStatus,
+  setGender,
+  setPagination,
+  setSpecie,
+} from "../../store/slice/AppSlice";
+import useSelectors from "../../hooks/useSelectors";
+
+const Accordion: FC = () => {
+  const dispatch = useAppDispatch();
+  const { selectedPage, selectCharactersStatus, selectGender, selectSpecie } =
+    useSelectors();
+  const handleStatus = (status: string): void => {
+    dispatch(
+      setCharactersStatus(selectCharactersStatus !== status ? status : "")
+    );
+    dispatch(setPagination(1));
+    dispatch(
+      fetchCharacters({
+        page: selectedPage,
+        status: selectCharactersStatus !== status ? status : "",
+        species: selectSpecie,
+        gender: selectGender,
+      })
+    );
+  };
+  const handleSpecie = (species: string): void => {
+    dispatch(setSpecie(selectSpecie !== species ? species : ""));
+    dispatch(setPagination(1));
+    dispatch(
+      fetchCharacters({
+        page: selectedPage,
+        status: selectCharactersStatus,
+        species: selectSpecie !== species ? species : "",
+        gender: selectGender,
+      })
+    );
+  };
+  const handleGender = (gender: string): void => {
+    dispatch(setGender(selectGender !== gender ? gender : ""));
+    dispatch(setPagination(1));
+    dispatch(
+      fetchCharacters({
+        page: selectedPage,
+        status: selectCharactersStatus,
+        species: selectSpecie,
+        gender: selectGender !== gender ? gender : "",
+      })
+    );
+  };
+  return (
+    <div className={style.AccordionRoot}>
+      <MUIAccordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Status</Typography>
+          {selectCharactersStatus && (
+            <span className={style.AccordionSelected}>
+              {selectCharactersStatus}
+            </span>
+          )}
+        </AccordionSummary>
+        <AccordionDetails>
+          {statusData.map((status) => (
+            <Button
+              variant={
+                selectCharactersStatus === status ? "contained" : "outlined"
+              }
+              size="small"
+              color={"success"}
+              sx={{ margin: "10px" }}
+              onClick={() => handleStatus(status)}
+            >
+              {status}
+            </Button>
+          ))}
+        </AccordionDetails>
+      </MUIAccordion>
+      <MUIAccordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Species</Typography>
+          {selectSpecie && (
+            <span className={style.AccordionSelected}>{selectSpecie}</span>
+          )}
+        </AccordionSummary>
+        <AccordionDetails>
+          {speciesData.map((specie) => (
+            <Button
+              variant={selectSpecie === specie ? "contained" : "outlined"}
+              size="small"
+              color={"success"}
+              sx={{ margin: "10px" }}
+              onClick={() => handleSpecie(specie)}
+            >
+              {specie}
+            </Button>
+          ))}
+        </AccordionDetails>
+      </MUIAccordion>
+      <MUIAccordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Genders </Typography>
+          {selectGender && (
+            <span className={style.AccordionSelected}>{selectGender}</span>
+          )}
+        </AccordionSummary>
+        <AccordionDetails>
+          {gendersData.map((gender) => (
+            <Button
+              variant={selectGender === gender ? "contained" : "outlined"}
+              size="small"
+              color={"success"}
+              sx={{ margin: "10px" }}
+              onClick={() => handleGender(gender)}
+            >
+              {gender}
+            </Button>
+          ))}
+        </AccordionDetails>
+      </MUIAccordion>
+    </div>
+  );
+};
+export default Accordion;
