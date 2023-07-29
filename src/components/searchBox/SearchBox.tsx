085c3130c796ba29ec12021c2b19b4c1afaf0067
Manouchehr Ghalevand <m.ghalevand@outlook.com>
2023-07-29 21:08:42 +0330

    Implemented responsive design for navBar, content, and accordion components. Used SCSS media queries for different screen sizes. Responsive menu for navBar, Flexbox for content, and optimized accordion design for mobile screens. Improves user experience on both desktop and mobile devices.

--- src/components/searchBox/SearchBox.tsx
+++ src/components/searchBox/SearchBox.tsx
@@ -1,4 +1,5 @@
 import Box from "@mui/material/Box";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import { TextField } from "@mui/material";
 
 import useSelectors from "../../hooks/useSelectors";
@@ -6,17 +7,17 @@ import { useAppDispatch } from "../../store/ConfigureStore";
 import { setSearchBox } from "../../store/slice/AppSlice";
 
 const SearchBox = () => {
+  const matches = useMediaQuery("(max-width:716px)");
   const dispatch = useAppDispatch();
   const { themeMode, selectSearchBox } = useSelectors();
-
   return (
     <Box
       sx={{
-        width: 350,
+        width: matches ? 270 : 350,
         maxWidth: "100%",
         position: "absolute",
         top: "45%",
-        left: "35%",
+        left: matches ? "13%" : "35%",
         backgroundColor: themeMode === "dark" ? "#0c0c0c" : "white",
       }}
     >
